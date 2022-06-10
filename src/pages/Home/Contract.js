2fd1d8ddf5adbd94c528b5b49acfc44c0abf20cf
nurulamin092 <mnurulamin092@gmial.com>
2022-06-10 22:10:41 +0600

    footer change

--- src/pages/Home/Contract.js
+++ src/pages/Home/Contract.js
@@ -20,10 +20,8 @@ const Contract = () => {
             <form ref={form} onSubmit={sendEmail}>
                 <div className=' px-10 py-14 bg-slate-900'>
                     <div className='text-center pb-14 text-white'>
-                        <p className='text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary'>
-                            Contact Us
-                        </p>
-                        <h1 className='text-4xl'>Stay connected with us</h1>
+
+                        <h1 className='text-4xl'> Contact Me</h1>
                     </div>
                     <div className='grid grid-cols-1 justify-items-center gap-5'>
                         <input
